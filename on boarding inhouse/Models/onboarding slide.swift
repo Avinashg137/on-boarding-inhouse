9352a6cdc62579c911c565552623b4a8915258ba
Avinashg137 <avinashg.itpath@gmail.com>
2022-03-15 10:58:08 -0700

    add next button

--- on boarding inhouse/Models/onboarding slide.swift
+++ on boarding inhouse/Models/onboarding slide.swift
@@ -11,5 +11,5 @@ import UIKit
 struct Onboardingslide {
     let title : String
     let description : String
-    let image: UIImage
+    let image: UIImage?
 }
--- on boarding inhouse/Views/Onboarding/OnboardingViewController.swift
+++ on boarding inhouse/Views/Onboarding/OnboardingViewController.swift
@@ -11,9 +11,11 @@ class OnboardingViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var nextbtn: UIButton!
-    @IBOutlet weak var pageController: UIPageControl!
+    
     
     var slides : [Onboardingslide ] = []
+    var currentPage = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +24,7 @@ class OnboardingViewController: UIViewController {
         Onboardingslide(title: " Become a better person", description: "Join a step by step guided emotional refinement journey sourced in Torah and Kabbalah", image: #imageLiteral(resourceName: "heart")),
         Onboardingslide(title: "Never miss a day.", description: "Get smart notifications, a progress tracker and blessings toggle to keep you up to speed.", image: #imageLiteral(resourceName: "calendar")),
         Onboardingslide(title: "Amplify your growth.", description: "Tap into daily meditations, exercises, private journal, required blessings, short videos, progress tracker, multi-languages.", image: #imageLiteral(resourceName: "water-drop")),
-    //    Onboardingslide(title: "Register your account", description: "Receive reminders and snippets of the daily omer exercises directly in your inbox.", image: nil)
+        Onboardingslide(title: "Register your account", description: "Receive reminders and snippets of the daily omer exercises directly in your inbox.", image: nil)
         ]
 
         // Do any additional setup after loading the view.
@@ -33,10 +35,22 @@ class OnboardingViewController: UIViewController {
     
 
     @IBAction func nextbtnclick(_ sender: Any) {
+        
+        
+        if currentPage > self.slides.count - 2 {
+            print("complete")
+            
+               return
+           }
+
+           let indexPath = IndexPath(item: currentPage + 1, section: 0)
+           collectionView.scrollToItem(at: indexPath, at: .right, animated: true)
+        currentPage += 1
     }
+    
 }
 
-extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return slides.count
@@ -47,4 +61,8 @@ extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDa
         return cell
         
     }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
 }
